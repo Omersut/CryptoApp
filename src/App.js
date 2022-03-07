3f9ed36f1e7123ea57e9491277d505daf542d859
Omer Sut <omersutcom@gmail.com>
2022-03-07 03:40:34 +0300

    contiunes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
 import List from "./components/List";
-import Wallet from "./components/Wallet";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<List />} />
-          <Route path="/wallet" element={<Wallet />} />
-        </Routes>
-      </Router>
+      <header className="App-header">
+        <List />
+      </header>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,111 +1,51 @@
-import { useEffect, useState } from "react";
-import { useList } from "../context/ListContext";
+import { useState, useEffect } from "react";
 
 function Search({
-  item,
-  key,
   name,
   image,
   symbol,
-  marketcap,
+  volume,
   price,
   priceChange,
-  volume,
+  marketcap,
 }) {
-  // context basket
-  const { addToList, items, removeFromList, usd, setUSD } = useList();
-  const findListItem = items.find((list_item) => list_item._id === key);
-  const [inWalled, setInwallet] = useState(false);
-
-  const Add = async () => {
-    if (usd > item.current_price) {
-      addToList(item);
-      setInwallet(true);
-      setUSD(parseInt(localStorage.getItem("usd")) - item.current_price);
-      localStorage.setItem("usd", JSON.stringify(usd));
-    }
-  };
-  const Remove = async () => {
-    removeFromList(item.id);
-    setInwallet(false);
-    setUSD(parseInt(localStorage.getItem("usd")) + item.current_price);
-    localStorage.setItem("usd", JSON.stringify(usd));
-  };
-
   return (
     <>
       <div
         style={{
-          width: "250px",
+          width: "270px",
 
           margin: "7px",
-          height: "250px",
+          height: "270px",
         }}
         className={
           priceChange < 0
-            ? "card border-danger mb-3"
-            : "card border-success mb-3"
+            ? "card text-white bg-danger mb-3"
+            : "card text-white bg-success mb-3"
         }
       >
-        <div className="card-header">{item.name}</div>
+        <div className="card-header">{name.slice(0, 12)}</div>
         <div className="card-body">
           <img
             style={{
-              height: "88px",
+              width: "70px",
             }}
-            src={item.image}
+            src={image}
             alt="crypto"
           />
-          <div style={{ marginTop: "12px" }}>
-            <h4
-              className="card-text"
-              style={{
-                marginTop: "25px",
-                display: "inline",
-              }}
-            >
-              <strong style={{ color: "black" }}> {item.current_price}</strong>
-            </h4>
-          </div>
-          <div
+
+          <h3 style={{ marginTop: "20px" }} className="card-title">
+            {priceChange.toFixed(2)}%
+          </h3>
+          <h3
+            className="card-text"
             style={{
-              marginTop: "8px",
-              display: "flex",
-              justifyContent: "center",
+              border: "solid 2px black",
+              marginTop: "25px",
             }}
           >
-            {!inWalled && (
-              <button
-                style={{ margin: "5px", height: "41px", width: "45%" }}
-                type="button"
-                className="btn btn-success"
-                onClick={Add}
-              >
-                BUY
-              </button>
-            )}
-            <button
-              style={{ margin: "5px", width: "45%" }}
-              type="button"
-              className={
-                priceChange < 0
-                  ? "btn btn-outline-danger"
-                  : "btn btn-outline-success"
-              }
-            >
-              {item.market_cap_change_percentage_24h.toFixed(1)}%
-            </button>
-            {inWalled && (
-              <button
-                style={{ margin: "5px", height: "41px", width: "45%" }}
-                type="button"
-                className="btn btn-danger"
-                onClick={Add}
-              >
-                ✗
-              </button>
-            )}
-          </div>
+            <strong style={{ color: "black" }}>$ {price}</strong>
+          </h3>
         </div>
       </div>
     </>
